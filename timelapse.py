7f662ef9f5899c90d901e6828b4d37a838056ec9
Terje Nesthus <terje@ekstremedia.no>
2019-07-07 22:30:21 +0200

    Autocommit

--- timelapse.py
+++ timelapse.py
@@ -1,2 +1,36 @@
 #!/usr/bin/env python
-print "Test"
+from datetime import datetime, date, time
+from picamera import PiCamera
+import os
+import time
+import sys
+import yaml
+config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
+def set_camera_options(camera):
+    # Set camera resolution.
+    if config['resolution']:
+        camera.resolution = (
+            config['resolution']['width'],
+            config['resolution']['height']
+        )
+    camera.iso = 100
+
+
+
+
+
+
+camera = PiCamera()
+set_camera_options(camera)
+
+
+print camera.iso
+
+
+
+
+
+
+
+
+
--- tl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Starting timelapse"
-raspistill -awb cloud -o still%05d.jpg -tl 10000 -t 86400000 -ex verylong
