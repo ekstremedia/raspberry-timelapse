15f39a2ded329d28d882854ae1b2620d13be79ab
Terje Nesthus <terje@ekstremedia.no>
2019-07-07 23:09:11 +0200

    Autocommit

--- timelapse.py
+++ timelapse.py
@@ -5,25 +5,37 @@ import os
 import time
 import sys
 import yaml
-config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
-def set_camera_options(camera):
-    # Set camera resolution.
 
+try:
+    config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
+    loadedConf = True
+except OSError as e:
+    print("Found no configuration file!")
+    loadedConf = False
+
+if loadedConf: 
+    def set_camera_options(camera):
+        # Set camera resolution.
+   
+        if config['resolution']:
+            camera.resolution = (
+                config['resolution']['width'],
+                config['resolution']['height']
+            )
+        camera.iso = 100
     
-    if config['resolution']:
-        camera.resolution = (
-            config['resolution']['width'],
-            config['resolution']['height']
-        )
-    camera.iso = 100
+        if config['isloaded']:
+            print("Configuration file loaded!")
+        else:
+            print("Configuration file not loaded!")
 
-    if config['isloaded']:
-        print("Configuration file loaded!")
-    else:
-        print("Configuration file not loaded!")
-
+# Initalize Camera
 camera = PiCamera()
-set_camera_options(camera)
+
+# Set camera config if config is loaded
+
+if loadedConf:
+    set_camera_options(camera)
 
 print(camera.iso)
 #rint dir(camera)
