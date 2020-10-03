e20cb72ce867b45d32a634f33d28ef0c4a8efd12
Terje Nesthus <terje@ekstremedia.no>
2020-10-03 18:52:53 +0200

    Autocommit

--- php/index.php
+++ php/index.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sortland Camera 01</title>
 </head>
+
 <body>
-        Sortland camera #01  - Reloading every 15 sec
-        <br />
-        <img src="../status.jpg" id="camera" class="responsive">
+    Sortland camera #01 - Reloading every 15 sec
+    <br /> <br />
+    <img src="../status.jpg" id="camera" class="responsive">
 </body>
 <style>
-body {
-    text-align: center;
-    color: hotpink;
-    background-color: #000;
-}
-.responsive {
-  width: 90%;
-  height: auto;
-}
+    body {
+        text-align: center;
+        color: hotpink;
+        background-color: #000;
+    }
+    .responsive {
+        border-radius: 5px;
+        border: 1px rgba(225,105,180,0.3) solid;
+        width: 90%;
+        height: auto;
+    }
 </style>
 <script>
-setInterval(function() {
-    var myImageElement = document.getElementById('camera');
-    myImageElement.src = '../status.jpg?rand=' + Math.random();
-}, 15000);
+    setInterval(function() {
+        var myImageElement = document.getElementById('camera');
+        myImageElement.src = '../status.jpg?rand=' + Math.random();
+    }, 15000);
 </script>
+
 </html>
