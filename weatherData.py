0f615be5167f5764b6db0c444a90a483a241238b
Ekstremedia <terje@ekstremedia.no>
2021-01-23 21:23:27 +0100

    Autocommit

--- weatherData.py
+++ weatherData.py
@@ -4,28 +4,40 @@ from os import path
 import subprocess
 
 filename = 'netatmo.json'
-if path.exists(filename):
-    print("File exists")
 if not path.exists(filename):
-    print("File doesnt exists, downloading")
+    #print("File doesnt exists, downloading")
     cmd = ['python', 'getWeather.py']
     subprocess.Popen(cmd).wait()
 
 with open(filename, 'r') as json_file:
     data = json.load(json_file)
 
-print(data['body']['devices'][0]['modules'][0]['dashboard_data']['Temperature'])
-print(data['body']['devices'][0]['modules'][0]['dashboard_data']['Humidity'])
-print(data['body']['devices'][0]['modules'][0]['dashboard_data']['temp_trend'])
+weatherList = {}
+
+temp = { 'temperature': data['body']['devices'][0]['modules'][0]['dashboard_data']['Temperature'] }
+humidity = { 'humidity': data['body']['devices'][0]['modules'][0]['dashboard_data']['Humidity'] }
+temp_trend = { 'temp_trend': data['body']['devices'][0]['modules'][0]['dashboard_data']['temp_trend'] }
+pressure = { 'pressure': data['body']['devices'][0]['dashboard_data']['Pressure'] }
+pressure_trend = { 'pressure_trend': data['body']['devices'][0]['dashboard_data']['pressure_trend'] }
+
+
+weatherList.update(temp)
+weatherList.update(humidity)
+weatherList.update(temp_trend)
+weatherList.update(pressure)
+weatherList.update(pressure_trend)
+
 try:
-    print(data['body']['devices'][0]['modules'][0]['dashboard_data']['min_temp'])
+    weatherList.update({'min_temp': data['body']['devices'][0]['modules'][0]['dashboard_data']['min_temp'] })
 except KeyError:
     print("No min temp yet")
 try:
-    print(data['body']['devices'][0]['modules'][0]['dashboard_data']['max_temp'])
+    weatherList.update({'max_temp': data['body']['devices'][0]['modules'][0]['dashboard_data']['max_temp'] })
 except KeyError:
     print("No min temp yet")
 try:
-    print(data['body']['devices'][0]['modules'][1]['dashboard_data']['Rain'])
+    weatherList.update({'rain': data['body']['devices'][0]['modules'][1]['dashboard_data']['Rain'] })
 except KeyError:
     print("No min temp yet")
+
+print(weatherList)
