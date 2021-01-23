17c60f948f4cb9e954d30eeb6b06944ab5e47aa4
Terje Nesthus <terje@ekstremedia.no>
2021-01-24 00:18:40 +0100

    Autocommit

--- getWeather.py
+++ getWeather.py
@@ -5,9 +5,9 @@ import logging
 import os
 from datetime import datetime, date, time
 homedir = "/home/pi/raspberry-timelapse/"
-logdir = os.path.join(homedir, "logs");
+logdir = os.path.join(homedir, "logs")
 logfilename = "getWeather.log"
-logfile = os.path.join(logdir, logfilename);
+logfile = os.path.join(logdir, logfilename)
 if not os.path.exists(logdir):
     os.makedirs(logdir)
     print("Made logs/ directory")
@@ -24,38 +24,33 @@ def log(text):
 filename = os.path.join(homedir, 'netatmo.json')
 url = 'https://ekstremedia.no/nesthus2020/public/data/netatmoStatus'
 try:
-  req = urllib.request.Request(url)
-  r = urllib.request.urlopen(req).read()
-  cont = json.loads(r.decode('utf-8'))
+    req = urllib.request.Request(url)
+    r = urllib.request.urlopen(req).read()
+    cont = json.loads(r.decode('utf-8'))
+    weatherList = {}
+    temp = {'temperature': cont['body']['devices'][0]['modules'][0]['dashboard_data']['Temperature']}
+    humidity = {'humidity': cont['body']['devices'][0]['modules'][0]['dashboard_data']['Humidity']}
+    temp_trend = {'temp_trend': cont['body']['devices'][0]['modules'][0]['dashboard_data']['temp_trend']}
+    pressure = {'pressure': cont['body']['devices'][0]['dashboard_data']['Pressure']}
+    pressure_trend = {'pressure_trend': cont['body']['devices'][0]['dashboard_data']['pressure_trend']}
+    co2 = {'co2': cont['body']['devices'][0]['dashboard_data']['CO2']}
 
-  weatherList = {}
+    weatherList.update(temp)
+    weatherList.update(humidity)
+    weatherList.update(temp_trend)
+    weatherList.update(pressure)
+    weatherList.update(pressure_trend)
+    weatherList.update(co2)
 
-  temp = {'temperature': cont['body']['devices'][0]['modules'][0]['dashboard_data']['Temperature']}
-  humidity = {'humidity': cont['body']['devices'][0]['modules'][0]['dashboard_data']['Humidity']}
-  temp_trend = {'temp_trend': cont['body']['devices'][0]['modules'][0]['dashboard_data']['temp_trend']}
-  pressure = {'pressure': cont['body']['devices'][0]['dashboard_data']['Pressure']}
-  pressure_trend = {'pressure_trend': cont['body']['devices'][0]['dashboard_data']['pressure_trend']}
-  co2 = {'co2': cont['body']['devices'][0]['dashboard_data']['CO2']}
+    logTemp = "Temp: "+str(weatherList['temp'])+"c"
+    logHumidity = "Humidity: "+str(weatherList['humidity'])
 
-  weatherList.update(temp)
-  weatherList.update(humidity)
-  weatherList.update(temp_trend)
-  weatherList.update(pressure)
-  weatherList.update(pressure_trend)
-  weatherList.update(co2)
-
-
-
-  logTemp = "Temp: "+str(weatherList['temp'])+"c"
-  logHumidity = "Humidity: "+str(weatherList['humidity'])
-
-  logOut = logTemp +" "+logHumidity
-  with open(filename, 'w') as outfile:
-    json.dump(cont, outfile)
-    log(logOut)
-
+    logOut = logTemp +" "+logHumidity
+    with open(filename, 'w') as outfile:
+        json.dump(cont, outfile)
+        log(logOut)
 except:
-  log("Could not download file")
+    log("Could not download file")
 
 
 
