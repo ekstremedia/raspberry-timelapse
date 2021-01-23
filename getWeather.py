186654b602e4c71dab6ea5bd12e5de8499991437
Terje Nesthus <terje@ekstremedia.no>
2021-01-23 23:45:44 +0100

    Autocommit

--- getWeather.py
+++ getWeather.py
@@ -27,10 +27,37 @@ try:
   req = urllib.request.Request(url)
   r = urllib.request.urlopen(req).read()
   cont = json.loads(r.decode('utf-8'))
-  #print(cont['body']['devices'][0]['station_name'])
-  temp = cont['body']['devices'][0]['modules'][0]['dashboard_data']['Temperature']
+
+  weatherList = {}
+
+  temp = {'temperature': cont['body']['devices'][0]['modules'][0]['dashboard_data']['Temperature']}
+  humidity = {'humidity': cont['body']['devices'][0]['modules'][0]['dashboard_data']['Humidity']}
+  temp_trend = {'temp_trend': cont['body']['devices'][0]['modules'][0]['dashboard_data']['temp_trend']}
+  pressure = {'pressure': cont['body']['devices'][0]['dashboard_data']['Pressure']}
+  pressure_trend = {'pressure_trend': cont['body']['devices'][0]['dashboard_data']['pressure_trend']}
+  co2 = {'co2': cont['body']['devices'][0]['dashboard_data']['CO2']}
+
+  weatherList.update(temp)
+  weatherList.update(humidity)
+  weatherList.update(temp_trend)
+  weatherList.update(pressure)
+  weatherList.update(pressure_trend)
+  weatherList.update(co2)
+
+
+
+  logTemp = "Temp: "+str(weatherList['temp'])+"c"
+  logHumidity = "Humidity: "+str(weatherList['humidity'])
+
+  logOut = logTemp +" "+logHumidity
   with open(filename, 'w') as outfile:
     json.dump(cont, outfile)
-    log("Temp: "+str(temp)+"c")
+    log(logOut)
+
 except:
   log("Could not download file")
+
+
+
+
+
