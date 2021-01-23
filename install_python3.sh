033ce26c7a999d736980ff9281a5684f5d98a295
Ekstremedia <terje@ekstremedia.no>
2021-01-23 18:03:06 +0100

    Autocommit

---
+++ install_python3.sh
@@ -0,0 +1,13 @@
+sudo apt-get install -y build-essential tk-dev libncurses5-dev \
+	libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev \
+	libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+
+version=3.8.5
+
+wget https://www.python.org/ftp/python/$version/Python-$version.tgz
+
+tar zxf Python-$version.tgz
+cd Python-$version
+./configure --enable-optimizations
+make -j4
+sudo make altinstall
