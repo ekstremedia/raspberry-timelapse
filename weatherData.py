304b3066025848340cde55e1301dbfa52ab3dedc
Ekstremedia <terje@ekstremedia.no>
2021-01-23 21:42:06 +0100

    Autocommit

--- weatherData.py
+++ weatherData.py
@@ -19,13 +19,14 @@ humidity = { 'humidity': data['body']['devices'][0]['modules'][0]['dashboard_dat
 temp_trend = { 'temp_trend': data['body']['devices'][0]['modules'][0]['dashboard_data']['temp_trend'] }
 pressure = { 'pressure': data['body']['devices'][0]['dashboard_data']['Pressure'] }
 pressure_trend = { 'pressure_trend': data['body']['devices'][0]['dashboard_data']['pressure_trend'] }
-
+co2 = { 'co2': data['body']['devices'][0]['dashboard_data']['CO2'] }
 
 weatherList.update(temp)
 weatherList.update(humidity)
 weatherList.update(temp_trend)
 weatherList.update(pressure)
 weatherList.update(pressure_trend)
+weatherList.update(co2)
 
 try:
     weatherList.update({'min_temp': data['body']['devices'][0]['modules'][0]['dashboard_data']['min_temp'] })
