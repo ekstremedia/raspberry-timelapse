63fa8ab5846ad9e60a2be83d7d8414858866bf6c
Terje Nesthus <terje@ekstremedia.no>
2021-01-25 02:09:30 +0100

    Autocommit

---
+++ astral.py
@@ -0,0 +1,7 @@
+from astral import LocationInfo
+city = LocationInfo("London", "England", "Europe/London", 51.5, -0.116)
+print((
+    f"Information for {city.name}/{city.region}\n"
+    f"Timezone: {city.timezone}\n"
+    f"Latitude: {city.latitude:.02f}; Longitude: {city.longitude:.02f}\n"
+))
