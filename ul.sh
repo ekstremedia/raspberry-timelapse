93e8cd90de1f0de1fab3dbb2a565978eab1cb2d9
Ekstremedia <terje@ekstremedia.no>
2021-01-30 02:43:27 +0100

    Autocommit

---
+++ ul.sh
@@ -0,0 +1,7 @@
+python youtubeUpload.py --file="/var/www/html/auto-output-25.mp4"
+                       --title="/var/www/html/auto-output-25.mp4"
+                       --description="timelapse test"
+                       --keywords="timelapse, test"
+                       --category="22"
+                       --privacyStatus="public"
+                       --noauth_local_webserver
