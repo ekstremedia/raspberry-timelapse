18750fcde183684401a481a54bbf82f2e1e78764
Terje Nesthus <terje@fdvhuset.no>
2021-02-06 11:12:09 +0100

    Autocommit

--- initVar.py
+++ initVar.py
@@ -3,5 +3,10 @@ from localStoragePy import localStoragePy
 
 localStorage = localStoragePy('ekstremedia-timelapse-exposure', 'sqlite')
 
-localStorage.setItem('countDown', 'on')
-localStorage.setItem('countUp', 'off')
+#localStorage.setItem('countDown', 'on')
+#localStorage.setItem('countUp', 'off')
+
+print(localStorage.getItem('countUp'))
+print(localStorage.getItem('countDown'))
+print(localStorage.getItem('currentExposure'))
+
