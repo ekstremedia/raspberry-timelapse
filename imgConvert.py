e912ed6f508c5f92e29efe26c0fa6243e5512b9c
Terje Nesthus <terje@fdvhuset.no>
2021-02-06 01:29:03 +0100

    Autocommit

--- imgConvert.py
+++ imgConvert.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env /usr/local/bin/python3.9
+
 import os
 import sys
 from shutil import copyfile
 import json
 import subprocess 
 from logger import *
+import pytz
+import datetime
+import yaml
+
+tz = pytz.timezone('Europe/Oslo')
+#mytz = datetime(2021,12,31)
+mytz = datetime.datetime.now()
+mytz = tz.localize(mytz)
+day = mytz.strftime ('%d .%b %Y %H:%M:%S')
+config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
+try:
+    annotation = config['annotation']
+except KeyError:
+    annotation = ""
+topText = f"{annotation} - {day}"
+
+
+# f = open(last_image_file, "w+")
+# f.write(f"/var/www/html/bilder/2021/02/05/sigerfjord_cam_01_20_21_26.jpg")
+# f.close()
+
+
+
 
 homedir = sys.path[0]
-last_image_file =  os.path.join(homedir, "logs/last_image.log");
+last_image_file = os.path.join(homedir, "logs/last_image.log");
 #last_shutterspeed =  os.path.join(homedir, "logs/last_shutter.log");
 shutter_cmd = os.path.join(homedir, 'Timer.py');
+print(last_image_file)
 with open(last_image_file) as f:
     filename = f.readline()
+print(filename)
 
 # with open(last_shutterspeed) as sh:
 #     last_shutter = sh.readline()
 last_shutter = subprocess.getoutput(shutter_cmd)
-
+#print(last_shutter)
 fileout = filename
 if os.path.exists(filename):
     netatmoFile = "netatmo.json"
@@ -65,6 +91,8 @@ if os.path.exists(filename):
     cmd = "convert "+ filename+ " -undercolor 'rgba(0,0,0,0.4)' -fill white -gravity NorthWest -pointsize 16 -annotate +10+120 \'" + logRain + "\' " + fileout
     os.system(cmd)
     cmd = "convert "+ filename+ " -undercolor 'rgba(0,0,0,0.4)' -fill white -gravity NorthWest -pointsize 16 -annotate +10+140 \'exposure: " + last_shutter + "\' " + fileout
+    os.system(cmd)
+    cmd = "convert "+ filename+ " -undercolor 'rgba(0,0,0,0.4)' -fill white -gravity North -pointsize 20 -annotate +5+5 \' " + topText + "\' " + fileout
     os.system(cmd)
     log("imgConvert done")
 else:
