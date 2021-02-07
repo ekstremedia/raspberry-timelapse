8065278634f0740bad850415689c546532f240db
Terje Nesthus <terje@ekstremedia.no>
2021-02-07 01:25:24 +0100

    Autocommit

--- piTemp.py
+++ piTemp.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env /usr/local/bin/python3.9
 import subprocess as sp
-
-temp  = sp.getoutput('/opt/vc/bin/vcgencmd measure_temp')
-print(temp)
+# temp  = sp.getoutput('/opt/vc/bin/vcgencmd measure_temp')
+temp  = sp.getoutput('cat /sys/class/thermal/thermal_zone0/temp')
+tempC = round(int(temp)/1000,1)
+print(tempC)
+# print(temp[5:-2]) 
