1380f1d79f26c815685a214a8587252e908a46bb
Terje Nesthus <terje@ekstremedia.no>
2021-02-19 22:50:54 +0100

    Autocommit

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,7 @@ sudo pip3 install pyyaml
 sudo pip3 install requests
 sudo pip3 install pytz
 python3.9 -m pip install pandas
+python3.9 -m pip install pyyaml
+python3.9 -m pip install picamera
+python3.9 -m pip install localStoragePy
+python3.9 -m pip install pytz
