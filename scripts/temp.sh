b8ca97440f3e51f5de7563f894d6714c0f44a8e3
Terje Nesthus <terje@ekstremedia.no>
2021-02-20 13:37:44 +0000

    Autocommit

---
+++ scripts/temp.sh
@@ -0,0 +1,9 @@
+cpu=$(</sys/class/thermal/thermal_zone0/temp)
+
+echo "$(date) @ $(hostname)"
+
+echo "-------------------------------------------"
+
+echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
+
+echo "CPU => $((cpu/1000))'C"
